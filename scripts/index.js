ae18b225c5021fb272ea3d52e4c5b4339b89ac3f
Rafael <rafadevjs@gmail.com>
2022-04-06 23:05:55 -0300

    style: create the knowledge section

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,40 @@
-let toggle = document.querySelector(".menu-toggle");
-let menu_mobile = document.querySelector(".menu-mobile");
+class Controller{
+    constructor(){
+        this.menuToggle();
+        this.hoverImageProject();
+    }
 
-toggle.addEventListener("click", ()=>{
-    toggle.classList.toggle("active");
-    menu_mobile.classList.toggle("active");
-});
+    menuToggle(){
+        let toggle = document.querySelector("#menu-toggle");
+        let menu_mobile = document.querySelector("#menu-mobile");
+
+        toggle.addEventListener("click", ()=>{
+            toggle.classList.toggle("active");
+            menu_mobile.classList.toggle("active");
+        });
+    }//Closing the menuToggle method. 
+
+    hoverImageProject(){
+        let img = document.querySelectorAll("[data-img]");
+        let hover = document.querySelectorAll("[data-hover]");
+
+        hover.forEach((item)=>{
+            item.addEventListener("mouseover", ()=>{
+                item.classList.toggle("hide");
+            });
+        });
+        hover.forEach((item)=>{
+            item.addEventListener("mouseout", ()=>{
+                item.classList.toggle("hide");
+            });
+        });
+
+
+
+    }
+}
+
+let controller = new Controller();
+
+
 
